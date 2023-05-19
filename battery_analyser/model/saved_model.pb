db212e52182391f48a212a0154bf03ca0c1368bd
Thiago Paiva <thiagohdaqw@gmail.com>
2023-05-19 18:19:47 -0300

    Adicionando modelo atualizado

---
+++ battery_analyser/model/saved_model.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bf97ea67b01c41bf3ced7af8510b56680c171972136e94a7068a669906b3f1d
+size 257141
--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-IMAGE_FILE="tests/imagem.jpg"
+poetry install
+
+IMAGE_FILE="/tmp/imagem.jpg"
 
 while true; do
     sleep 0.5s;
 
-    ffmpeg -y -f video4linux2 -i /dev/video0 -vframes 1 \
+    ffmpeg -y -i /dev/video0 -vframes 1 \
          -video_size 640x480 $IMAGE_FILE 2>/dev/null > /dev/null
+
     if [[ "$?" != "0" ]]; then
         echo "Falha ao capturar webcam";
         exit 1;
